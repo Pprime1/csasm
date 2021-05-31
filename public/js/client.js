65edf67e480ac792d38fb246b63c0bd16ad00f2e
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-05-31 19:39:55 +1000

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -53,7 +53,10 @@ socket.on("room-update", (group_id, new_player_count) => {
 socket.on("room-display-update", (display_information) => {
   console.log(display_information);
   var MYID = socket.id // this is current player?
-  var DTStamp = display_information[0].updated_at; //display_information[0].updated_at.toLocaleString(); // how to convert the timestamp to a readable format?
+  
+  DTStamp = display_information[0].updated_at.toLocaleTimeString('en-GB');
+	
+ // var DTStamp = display_information[0].updated_at; //display_information[0].updated_at.toLocaleString(); // how to convert the timestamp to a readable format?
 	
   var $table = "<table border='1'> <caption>Current Player: " + MYID + " at " + DTStamp + "</caption>"
       $table += "<thead><tr><th>Player</th><th>Waypoint</th><th>Radius</th><th>Distance</th></tr></thead><tbody>"
