e6a721c57b00d369d2a7ca345e0f3b4953ad106b
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-05-31 15:25:44 +1000

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ function roomUpdateHandler(roomId, io){
       SELECT pl.id, pl.room_id, pl.updated_at,
          wp.name, wp.radius, round(ST_DISTANCE(wp.location, pl.location) * 100000) as "distance"
       FROM player as pl, waypoint as wp
-      WHERE wp.game_code = '${game_code}' AND pl.room_id = '${roomId.replace("group-", "")}' GROUP BY pl.id
+      WHERE wp.game_code = '${game_code}' AND pl.room_id = '${roomId.replace("group-", "")}' 
+      SORT BY pl.id
     `
 
     db_connnection.query(display_query).then(result => {
