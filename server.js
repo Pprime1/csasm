a1e6b84471360e9e503974058074e2838cf2803f
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-06-01 12:02:04 +1000

    Update server.js

--- server.js
+++ server.js
@@ -46,11 +46,11 @@ function roomUpdateHandler(roomId, io){
           // if count (waypoints.occupied) = waypoint.length then success = true
 
         // db_connnection.query("SELECT COUNT(name) AS n FROM waypoint");
-        var n = db_connnection.query("SELECT COUNT(*) as total FROM waypoint", function(err,result) {
-                // console.log(result); // What do I get here?
+        int n = db_connnection.query("SELECT COUNT(*) as total FROM waypoint", function(err,result) {
+                console.log(result.total); // What do I get here?
                 return parseInt(result.total);
         });
-        console.log("There are", n, "or", result.total, "waypoints to occupy"); // n is 'undefined'
+        console.log("There are", n, "waypoints to occupy"); // n is 'undefined'
         
         var m = 0;
         var wpcheck = []; 
