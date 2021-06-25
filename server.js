52bfc89ae67f368fbc6461fdf7760f728b8abd28
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-06-25 20:14:33 +1000

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ async function configure_socketio(db_connection) {
 	let game_code = roomId.replace("game-", "");   // merging game and room_id, but needs validity checking subroutine
         let game_query = `SELECT description as gamedescription FROM games WHERE game_code = '${game_code}'`;
         // let game_exists_query = `SELECT description FROM games WHERE EXISTS (SELECT game_code FROM games WHERE game_code = '${game_code}')`;
-        let game_query_result = await db_connection.query(game_query);
+        let game_query_result = db_connection.query(game_query);
         console.log("Description is", game_query_result[0].gamedescription);
 	// (node:21) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)
         // (node:21) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
