d1dc051c41a7b8fad897aa14a761a3bb9401420a
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-07-05 20:00:58 +1000

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -52,11 +52,10 @@ socket.io.on("reconnect", () => { // Reconnect is not used any more?
 socket.on("game-join", () => {
 // socket.on("game-join", (gamedesc) => {
    $("#lj-startup").hide();
-   $("#lj-reward").hide();
    $("#lj-in-game").show();
    navigator.geolocation.getCurrentPosition(updatePosition, PosError); // First location update attempt, handle errors
    const interval = setInterval(function() {
-       navigator.geolocation.getCurrentPosition(updatePosition); // TODO: does this really keep running ? Or should this bit be in room-update isntead?
+       navigator.geolocation.getCurrentPosition(updatePosition); // update geolocation every 5 seconds (is also updating every 10 seconds ... room-update??
    }, 5000);
 }); // end of GAME-JOIN
 
@@ -76,7 +75,7 @@ socket.on("display-update", (display_information) => {
   var MYID = socket.id; // this is current player
   var DTStamp = new Date(display_information[0].updated_at).toLocaleTimeString('en-GB'); // Last Room update timestamp
   // $("#game-description").text(gamedesc); // Current gamedescription
-  console.log("Current #game-description (display_update):", $("#game-description"));
+  console.log("Current #game-description (display_update):", $("#game-description").innertext);
 
   // Display game status including any occupied status lines
   var $table = "<table border='1'> <caption>Current Player: " + MYID + " at " + DTStamp + "</caption>"
@@ -123,7 +122,7 @@ window.addEventListener("load",function(event) {
      socket.emit('join-a-game', game, (response) => {
         console.log(response.status, response.message); // IF response.status==error then don't start a game keep the form open (the error message is put on screen to say so
         $("#game-description").text(response.message); // Set current gamedescription for display - NOT WORKING outside of this event listener?
-        // console.log("Current #game-description (in form):", $("#game-description"));
+        console.log("Current #game-description (in form):", $("#game-description"));
         // localStorage.setItem('current-game', game);
         // localStorage.setItem('game-description', response.message);     
      }); // emit join-a-game
