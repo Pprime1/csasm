1a9d5858d58b30c0a79796db4aeafef2afe3d2c0
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-07-29 09:00:23 +1000

    Update showmap.js

--- public/js/showmap.js
+++ public/js/showmap.js
@@ -44,7 +44,7 @@
      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
          id: 'mapbox/streets-v11',
-         //tileSize: 512,
+         tileSize: 1024,
          zoomOffset: -1,
          accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGdsNTYxcTR2MnB0amYzNnd1OHRhIn0.kcfA6jL1Be-qidECml4O4w'
      }).addTo(mymap);
