14b09002f497d883518256a8d726be8029cb06c2
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-07-29 08:57:36 +1000

    Update showmap.js

--- public/js/showmap.js
+++ public/js/showmap.js
@@ -1,7 +1,9 @@
 //--- TODO - display a map centred on the current player's geolocation, with usual controls in place for zoom/pan etc. Update the map as the player moves
 	//Display a marker showing the location of the current user
 	//Display circles showing the waypoints and their target radius (not too opaque). Popup the name and coordinates of each waypoint on mouse click/hover/something.
-     
+
+// why doesn't the map tiles show in the window? Only showing top left until I pan the display?
+
      //--- Alternative: Use straight openstreetmap. Not really any better? no good at all actually
      //L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
      //   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -31,11 +33,18 @@
 // latitude, longitude == of current player
 // waypoint name, lat, lon, radius for each waypoint in an array
 
-     var mymap = L.map('mapid').setView([latitude, longitude], 18);
+// var maparray = [];
+// maparray.push(L.marker(playerLoc));
+// var mapgroup = new L.featureGroup(maparray).addTo(mymap);
+// mapgroup.union(waypointcircles.getBounds());
+// mymap.fitBounds(mapgroup.getBounds());
+
+
+     var mymap = L.map('mapid').setView([latitude, longitude], 15);
      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
          id: 'mapbox/streets-v11',
-         tileSize: 512,
+         //tileSize: 512,
          zoomOffset: -1,
          accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGdsNTYxcTR2MnB0amYzNnd1OHRhIn0.kcfA6jL1Be-qidECml4O4w'
      }).addTo(mymap);
