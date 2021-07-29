91d4c457cca9add0ccbce6f248f7384b1f81b6ec
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-07-29 17:43:12 +1000

    Update showmap.js

--- public/js/showmap.js
+++ public/js/showmap.js
@@ -41,14 +41,16 @@
      var streetmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
          id: 'mapbox/streets-v11',
-         tileSize: 512,
+         maxZoom: 18,
+	 tileSize: 256,
          zoomOffset: -1,
          accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGdsNTYxcTR2MnB0amYzNnd1OHRhIn0.kcfA6jL1Be-qidECml4O4w'
      }),
      satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
          id: 'mapbox/satellite-v9',
-         tileSize: 512,
+         maxZoom: 18,
+	 tileSize: 512,
          zoomOffset: -1,
          accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGdsNTYxcTR2MnB0amYzNnd1OHRhIn0.kcfA6jL1Be-qidECml4O4w'
      });
