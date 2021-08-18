84f905e3c329492916ba89ebe9b5be829f566d47
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-08-18 14:00:15 +1000

    Update showmap.js

--- public/js/showmap.js
+++ public/js/showmap.js
@@ -88,9 +88,9 @@ var baseMaps = {
 //}).setView([latitude, longitude],14);
 
 var mymap = L.map('mapid').setView([latitude, longitude],14);
-mapLink = 'a href="http://openstreetmap.org">OpenStreetMap</a>';
+mapLink = 'a href="https://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
      attribution: '&copy; ' + mapLink + ' Contributors',
      maxZoom: 18,
 }).addTo(mymap);
