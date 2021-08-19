8345e1f0d4bdad19c44d5c8bb0a70ae4cfdc444e
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-08-19 15:20:07 +1000

    Update showmap.js

--- public/js/showmap.js
+++ public/js/showmap.js
@@ -1,19 +1,17 @@
-//--- TODO - display a map centred on the current player's geolocation, with usual controls in place for zoom/pan etc. Update the map as the player moves
-	//Display a marker showing the location of the current user
-	//Display circles showing the waypoints and their target radius (not too opaque). Popup the name and coordinates of each waypoint on mouse click/hover/something.
+//Display a map centred on the current player's geolocation, with usual controls in place for zoom/pan etc. Update the map as the player moves
+     //Display a marker showing the location of the current user
+     //Display circles showing the waypoints and their target radius (not too opaque). Popup the name and coordinates of each waypoint on mouse click/hover/something.
+     //TODO: Can we delay the first time this runs until after ten seconds or soemthing?
 
-// *** Global variables of note out of client.js ***
-// latitude, longitude == of current player  **** NOT UPDATING. IS HARD SET TO THE PRE-USER-UPDATE VALUES OF -27,153 ****
-// displaytable == array per waypoint of: pl.id, pl.room_id, pl.updated_at, wp.name, wp.radius, wp.location, round(ST_DISTANCE(wp.location, pl.location) * 100000) as "distance"
+// *** Global variables out of client.js ***
+    // latitude, longitude == geolocation of current player
+    // displaytable == array per waypoint of: pl.id, pl.room_id, pl.updated_at, wp.name, wp.radius, wp.location, round(ST_DISTANCE(wp.location, pl.location) * 100000) as "distance"
+var WPcircle=[]; // Store all game waypoints shown as map circles
 
-//FAIL - wp.location is in weird format: location: "0101000020110F00003A0664AF77473BC037548CF3371F6340"
-
-
-function updatemap() {
-   // Update the current player location 
-   PlayerLoc.setLatLng(latitude, longitude);
-   console.log("Current Player",MYID,latitude,longitude)
-   //--- display the player's direction of travel/facing? how? Not a feature it seems
+function updatemap() {  // Update the current player location on map
+   playerLoc.setLatLng(latitude, longitude); //update current player marker instead of creating new ones
+   console.log("Current Player:",MYID,latitude,longitude)
+   //--- TODO: display the player's direction of travel/facing? how? Not a feature it seems :-(
 
    // ZOOM: create an array of the objects and zoom the map to show them all?
    // var maparray = [];
@@ -27,31 +25,31 @@ function updatemap() {
    mymap.invalidateSize(); //reset map view
 }; // end updatemap
 
-
+//***SHOW MAP***
 // FAIL - why doesn't the map tiles show in the window? Only showing top left until I manually pan the display?
 
-// Allow streetview and satellite view on the map
+// Define streetview and satellite layer views on the map
 var streetmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-         id: 'mapbox/streets-v11',
-         maxZoom: 18,
-	 tileSize: 256,
-         zoomOffset: -1,
-         accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGRlenM3enRlMnRsM2s3NXl4cGRyIn0._X0tZf-JwyMdPCtZ8WHAMw' //public token
-	// accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGdsNTYxcTR2MnB0amYzNnd1OHRhIn0.kcfA6jL1Be-qidECml4O4w' //my token
-  }),
-  satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-         id: 'mapbox/satellite-v9',
-         maxZoom: 18,
-	 tileSize: 512,
-         zoomOffset: -1,
-	  accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGRlenM3enRlMnRsM2s3NXl4cGRyIn0._X0tZf-JwyMdPCtZ8WHAMw' //public token
-         // accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGdsNTYxcTR2MnB0amYzNnd1OHRhIn0.kcfA6jL1Be-qidECml4O4w' //my token
-     });
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: 'mapbox/streets-v11',
+      maxZoom: 18,
+      tileSize: 256,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGRlenM3enRlMnRsM2s3NXl4cGRyIn0._X0tZf-JwyMdPCtZ8WHAMw' //public token
+      // accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGdsNTYxcTR2MnB0amYzNnd1OHRhIn0.kcfA6jL1Be-qidECml4O4w' //my token
+   }),
+   satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: 'mapbox/satellite-v9',
+      maxZoom: 18,
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGRlenM3enRlMnRsM2s3NXl4cGRyIn0._X0tZf-JwyMdPCtZ8WHAMw' //public token
+      // accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGdsNTYxcTR2MnB0amYzNnd1OHRhIn0.kcfA6jL1Be-qidECml4O4w' //my token
+   });
 var baseMaps = {
-	"Streetmap": streetmap,
-	"Satellite": satellite
+     "Streetmap": streetmap,
+     "Satellite": satellite
 };
 
 // Initial display of map centred on the current player (or coords -27,153 as the startup values?)
@@ -59,12 +57,10 @@ var baseMaps = {
 //	layers: [streetmap]
 //}).setView([latitude, longitude],14);
 
-//Simplify it by just using a single layer and reverting to Openstreetmaps
+//Simplify it by just using a single layer and reverting to Openstreetmaps no special tiles
 var mymap = L.map('mapid').setView([latitude, longitude],14);
-mapLink = 'a href="https://openstreetmap.org">OpenStreetMap</a>';
-L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-     attribution: '&copy; ' + mapLink + ' Contributors',
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>',
      maxZoom: 18,
 }).addTo(mymap);
 
@@ -78,12 +74,11 @@ L.control.scale().addTo(mymap); //show scale bar
 // Display the current player location 
 var playerLoc = L.marker([latitude,longitude]) //current player location ... this isn't updating each time around, it is creating a new extra playerloc marker?
     .addTo(mymap)
-    .bindPopup("<b>Current Player</b><br>" + MYID + "<br>" +latitude + longitude);
+    .bindPopup("<b>Current Player</b><br>" + MYID + "<br>" +latitude + ", " + longitude);
     
-//--- display each waypoint and target radius as a circle
-var WPcircle=[]
-for (var i = 0; i < displaytable.length; i++) { 
-     if (displaytable[i].distance != null) { // if it is null there is an error somewhere
+//--- display each waypoint and target radius as a circle ... need to delay this until displaytable is set
+if (displaytable[i].distance != null) { // once populated display the circles
+   for (var i = 0; i < displaytable.length; i++) { 
         console.log("Targetting:", displaytable[i].name, displaytable[i].location, displaytable[i].radius);
         //  WPcircle[i] = L.circle(displaytable[i].location, {  // location doesn't appear to be in a usable format here
         //      color: 'red',
@@ -91,13 +86,16 @@ for (var i = 0; i < displaytable.length; i++) {
         //      fillOpacity: 0.25,
         //      radius: displaytable[i].radius
         // }).addTo(mymap);
-    
+
+//FAIL - wp.location is in weird format: location: "0101000020110F00003A0664AF77473BC037548CF3371F6340"
+
         //--- for each circle clicking on it will display the centre coordinates
         // WPcircle[i].bindPopup(displaytable[i].name + "<br>" + displaytable[i].location);
-    } else {
-	console.log("Target table is null", displaytable)  //why? how? should never occur
-    };
-}; //For each waypoint
+   }; //For each waypoint
+} else {
+    console.log("Target displaytable is null", displaytable)  // if not yet populated wait a bit and try again?
+};
+
 
 // I keep getting lost because map tiles aren't being displayed, click on map to see where you are 
 // *** not needed in final release (although clicking on objects will popup some info)
@@ -111,6 +109,7 @@ function onMapClick(e) {
 mymap.on('click', onMapClick);
 
 
+
 const interval = setInterval(function() {
           updatemap()
-}, 10000); // update map every 10 seconds with current player location.
+}, 1000); // update map every second with current player location.
