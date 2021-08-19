a645c9b52f61436255c66b57f915d9ee0923f619
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-08-19 21:12:06 +1000

    Update showmap.js

--- public/js/showmap.js
+++ public/js/showmap.js
@@ -37,26 +37,6 @@ L.control.layers(baseMaps).addTo(mymap); //show choice of layer views
 L.control.scale().addTo(mymap); //show scale bar
 mymap.invalidateSize(); //reset map view
    
-//--- display each waypoint and target radius as a circle ... need to delay this until displaytable is set
-console.log("Circles",displaytable);
-if (displaytable) { // once populated display the circles
-   for (var i = 0; i < displaytable.length; i++) { 
-        console.log("Target:", displaytable[i].name, displaytable[i].location, displaytable[i].radius);
-        //  WPcircle[i] = L.circle(displaytable[i].location, {  // location doesn't appear to be in a usable format here
-        //      color: 'red',
-        //      fillColor: '#f03',
-        //      fillOpacity: 0.25,
-        //      radius: displaytable[i].radius
-        // }).addTo(mymap);
-//FAIL - location is in weird format: location: "0101000020110F00003A0664AF77473BC037548CF3371F6340"
-
-        //--- for each circle clicking on it will display the centre coordinates
-        // WPcircle[i].bindPopup(displaytable[i].name + "<br>" + displaytable[i].location);
-   }; //For each waypoint
-} else {
-    console.log("Target displaytable is null", displaytable)  // if not yet populated wait a bit and try again?
-};
-
 const interval = setInterval(function() {
           updatemap()
 }, 5000); // update map every 5 seconds with current player location.
@@ -72,6 +52,26 @@ function updatemap() {  // Update the current player location on map
    };                  
    //--- TODO: display the player's direction of travel/facing? how? Not a feature it seems :-(
 
+   //--- display each waypoint and target radius as a circle ... need to delay this until displaytable is set
+   console.log("Circles",displaytable);
+   if (displaytable) { // display the circles only once populated
+       for (var i = 0; i < displaytable.length; i++) { 
+           console.log("Target:", displaytable[i].name, displaytable[i].location, displaytable[i].radius);
+           //  WPcircle[i] = L.circle(displaytable[i].location, {  // location doesn't appear to be in a usable format here
+           //      color: 'red',
+           //      fillColor: '#f03',
+           //      fillOpacity: 0.25,
+           //      radius: displaytable[i].radius
+           // }).addTo(mymap);
+//FAIL - location is in weird format: location: "0101000020110F00003A0664AF77473BC037548CF3371F6340"
+
+           //--- for each circle clicking on it will display the centre coordinates
+           // WPcircle[i].bindPopup(displaytable[i].name + "<br>" + displaytable[i].location);
+       }; //For each waypoint
+    } else {
+        console.log("Target displaytable is null", displaytable)  // if not yet populated wait a bit and try again?
+    };
+	
    // ZOOM: create an array of the objects and zoom the map to show them all?
    // var maparray = [];
    // maparray.push(L.marker(playerLoc));
