1616bad067d8b265635433d0df809ab97fe3fb85
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-09-26 15:27:52 +1000

    Update showmap.js

--- public/js/showmap.js
+++ public/js/showmap.js
@@ -33,7 +33,11 @@ var mymap = L.map('mapid', {
 }).setView([latitude, longitude],17);
 L.control.layers(baseMaps).addTo(mymap); //show choice of layer views
 L.control.scale().addTo(mymap); //show scale bar
-  
+ 
+var playerLoc = new L.marker([latitude,longitude]) //mark current player location
+    .addTo(mymap)
+    .bindPopup("<b>Current Player</b><br>" + MYID + "<br>" +latitude + ", " + longitude);
+
 //--- display each waypoint and target radius as a circle ... need to delay this until displaytable is set
 //console.log("Circles",displaytable);
 //if (displaytable) { // display the circles only once populated
@@ -55,13 +59,13 @@ L.control.scale().addTo(mymap); //show scale bar
  //  };
 	
 //Test a static circle ... why does this not show at all?
-var WPcircletst = L.circleMarker([-27.2792, 152.975867], {
+var WPcircletst = new L.circle([-27.2792, 152.975867], {
        radius: 150,
-       color: green,
-       fillColor: green,
-       fillOpacity: 0.25
+       //color: green,
+       //fillColor: green,
+       //fillOpacity: 0.25
 }).addTo(mymap);
-WPcircletst.bindPopup("HOME CIRCLE" + "<br>" + "location");
+//WPcircletst.bindPopup("HOME CIRCLE" + "<br>" + "location");
        
 
 function updatemap() {  // Update the current player location on map
