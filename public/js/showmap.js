451f143fdf465d841bf62db1d95ef74360300747
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-11-03 11:49:59 +1000

    Update showmap.js

--- public/js/showmap.js
+++ public/js/showmap.js
@@ -23,7 +23,7 @@ var streetmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
       maxZoom: 20,
       accessToken: 'pk.eyJ1IjoicHByaW1lMSIsImEiOiJja3JuNGdsNTYxcTR2MnB0amYzNnd1OHRhIn0.kcfA6jL1Be-qidECml4O4w' //my token
    }),
-   qglobe = L.tileLayer('https://spatial-img.information.qld.gov.au/arcgis/services/Basemaps/LatestStateProgram_AllUsers/ImageServer/{id}/tiles/{z}/{x}/{y}', {
+   qglobe = L.tileLayer('https://spatial-img.information.qld.gov.au/arcgis/services/Basemaps/LatestStateProgram_AllUsers/ImageServer/tiles/{z}/{x}/{y}', {
       attribution: 'Map data &copy; <a href="https://qldglobe.information.qld.gov.au/"</a>',
       maxZoom: 20
     });
