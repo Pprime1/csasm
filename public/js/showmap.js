49f723e27e949bf4520ca373c2a87de4e3211b0c
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-11-03 12:57:15 +1000

    Update showmap.js

--- public/js/showmap.js
+++ public/js/showmap.js
@@ -76,6 +76,14 @@ var panbtn = L.easyButton({
 
 mymap.on("zoomstart", function (e) { currentAutoMove = true }); //Set flag, that currently map is moved by a zoom command
 mymap.on("zoomend", function (e) { currentAutoMove = false }); //Remove flag again
+
+mymap.on('touchmove', (e) => {
+  if (!e.touches || e.touches.length !== 2) { return; }
+    if(!currentAutoMove){ //ignore if it was a natural PlayerLoc or programmatic update
+	    pauseAutoMove = true; //set flag to stop Auto moving map 
+	    panbtn.state('pauseAutoMove'); //change button style to remove crosshairs and have a arrow-in icon
+    }
+});
 mymap.on('movestart',(e)=>{ //Check if map is being moved
     if(!currentAutoMove){ //ignore if it was a natural PlayerLoc or programmatic update
 	    pauseAutoMove = true; //set flag to stop Auto moving map 
