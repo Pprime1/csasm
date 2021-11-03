8352a855e56cf6fafd3098c1b5c936ec1427eaee
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2021-11-03 12:38:37 +1000

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,16 +1,17 @@
 //***CLIENT.js***//
 const socket = io(); // or io("/"), the main namespace
 const urlParams = new URLSearchParams(location.search);
-var latitude =-27.5;    // make available to global variable in Displaymap.js
-var longitude =153;   // make available to global variable in Displaymap.js
+var latitude =-27.5;    // defaults to UQ St Lucia
+var longitude =153;   // defaults to UQ St Lucia
 var is_joined = false; // status for being part of a game
-var is_running = false; // status for once all main variables are first populated
-var displaytable =[]; // make available to global variable in Displaymap.js
-var MYID = socket.id; // this is current player, make available to global variable in Displaymap.js
+var map_started = false; //set once the map has been started up
+var game_description = "Clear skies: choose a game to play";
+var displaytable =[]; 
+var MYID = socket.id; 
+
 for (var entry of urlParams) { 
-    var URLentry = entry[0]; // only the first URL paramis considered as the Game ID code
+    var URLentry = entry[0]; // only the first URL param is considered as the Game ID code
 };   
-var RtnError = null;
 if (URLentry) {  //if started with a URLParam then attempt to join that game ID
     URLentry = URLentry.toUpperCase();
     console.log("Called with parameter:", URLentry);
@@ -19,6 +20,7 @@ if (URLentry) {  //if started with a URLParam then attempt to join that game ID
      }); // emit join-a-game
 } else { URLentry = "GCALPHATST" }; // set a default to simplify testing. Revert this to } else { URLentry = "" }; once released
 
+var RtnError = null;
 var geoOptions = {
   enableHighAccuracy: true,
   timeout: 5000,
@@ -40,8 +42,8 @@ function ConvertDEGToDM(deg,dir) {
   return direction + degrees + "° " + minutesdecimals+ "' ";
  }; // Convert DM.MMM
 
-function updatePosition(position) { //changes the player location details in:
-  latitude = position.coords.latitude; //here as a global variable
+function updatePosition(position) { //changes the player location details:
+  latitude = position.coords.latitude; //main variables
   longitude = position.coords.longitude;
   var accuracy = position.coords.accuracy;
   var lat = ConvertDEGToDM(latitude,1); //change display format
@@ -50,12 +52,12 @@ function updatePosition(position) { //changes the player location details in:
   $("#current-Lat").text(lat); //index.ejs for display above the table
   $("#current-Lon").text(lon);
   $("#current-Acc").text(acc);
-  localStorage.setItem('my_lat', lat); //localstorage for ease of access in reward.ejs and elsewhere
+  localStorage.setItem('my_lat', lat); //localstorage for ease of access in reward.ejs
   localStorage.setItem('my_lon', lon);
   localStorage.setItem('my_acc', accuracy);
   socket.emit('location-update', latitude, longitude); //server.js, which updates database for use in display-update later
   console.log("watchposition change", lat,lon);
-  if (map_started) {updatemap()}; //update the map display if the map has been created already
+  if (map_started) {updatemap()}; //the map display - if the map has been created already
     //??updatemap(latitude,longitude,displaytable)
 }; // UpdatePosition
 
@@ -97,15 +99,14 @@ socket.io.on("reconnect", () => { // Reconnect is not used any more?
 });
 
 socket.on("game-join", () => {
-   navigator.geolocation.getCurrentPosition(updatePosition, PosError, geoOptions); // First location update attempt, handle errors and set options
-   if (RtnError) {console.log("Geolocation Error Status", RtnError);}
-   if (!RtnError) { 
+   navigator.geolocation.getCurrentPosition(updatePosition, PosError, geoOptions); // First location update attempt, handle errors
+   if (RtnError) {console.log("Geolocation Error Status", RtnError)
+   } else { 
        $("#lj-startup").hide();
        $("#lj-in-game").show();
        is_joined = true; 
        navigator.geolocation.watchPosition(updatePosition, PosError, geoOptions); //keep updating geolocation when it changes, rather than on a loop
-       // put navigator.geolocation.clearWatch(???); into reward.ejs? or the call to reward in server.js perhaps?
-   }; //Get and set Player GeoLocation
+   }; //Get and set Player GeoLocation, and keep watch on it moving
 }); // end of GAME-JOIN
 
 socket.on("room-update", (game_id, gamedesc, new_player_count) => {
@@ -113,8 +114,9 @@ socket.on("room-update", (game_id, gamedesc, new_player_count) => {
   $("#lj-startup").hide();
   $("#current-game-id").text(game_id);
   if (gamedesc != 1) { // don't update if this was called by a room-leave command
-    localStorage.setItem('game_description', gamedesc);
-    localStorage.setItem('current_game', game_id);
+    localStorage.setItem('game_description', gamedesc); //?
+    localStorage.setItem('current_game', game_id); //?
+    game_description = gamedesc;
   };
   console.log(new_player_count, "are joined to", game_id, ":", gamedesc);  
   $("#current-game-player-count").text(new_player_count);
@@ -122,10 +124,10 @@ socket.on("room-update", (game_id, gamedesc, new_player_count) => {
 }); // end of ROOM-UPDATE
 
 socket.on("display-update", (display_information) => {
-  displaytable=display_information; // make available to global variable in Displaymap.js
+  displaytable=display_information; // make available as global variable in showmap.js
   MYID = socket.id; // this is current player
   var DTStamp = new Date(display_information[0].updated_at).toLocaleTimeString('en-GB'); // Last Room update timestamp
-  var game_description = localStorage.getItem ('game_description');
+  //game_description = localStorage.getItem ('game_description');
   $("#gamedesc").text(game_description);
 
   // Display game status including any occupied status lines
@@ -148,22 +150,23 @@ socket.on("display-update", (display_information) => {
   };
   $table += "</tr></tbody></table>";
   $('#displayinfo').empty().append($table);
-  localStorage.setItem('display_update', $table);
-  is_running = true; //no longer required?
-  if (!map_started) {startupmap()}; //startup the map, but just the first time we get here;
+  localStorage.setItem('display_update', $table); //needed in reward.ejs later? Could this be moved to the display-reward function?
+  if (!map_started) {startupmap();
+       map_started=true}; //startup the map, but just the first time we get here;
 
     //??startupmap(latitude,longitude,displaytable,MYID)
+    //map_started is a global variable out of showmap.js now
     
 }); // end of DISPLAY-UPDATE
 
 socket.on("display-reward", (reward_information) => { //if all waypoints are in occupied state, show Success! ONLY SENT TO VALID PLAYERS
-  localStorage.setItem('reward_information', reward_information); // Save Reward in Local Storage
+  localStorage.setItem('reward_information', reward_information); // Save Reward Info into Local Storage
   setTimeout( function() {
     location.href = "reward"; // Redirect user to reward page, disconnecting them from game and any session updates.
   }, 100)
 }); // end of DISPLAY-REWARD
 
-// Bind Submit Event for Front Page Game Joining form.
+// Bind Submit Event for Start Page Game-Joining form.
 window.addEventListener("load",function(event) {
   document.querySelector("#gameId").value = URLentry;
   var GmError = localStorage.getItem('RtnError') || "Clear skies";
