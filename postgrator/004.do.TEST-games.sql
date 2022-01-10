40daab694c352665cd156dbe9472f497b6de67c6
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2022-01-10 20:42:45 +1000

    Update 004.do.TEST-games.sql

--- postgrator/004.do.TEST-games.sql
+++ postgrator/004.do.TEST-games.sql
@@ -25,17 +25,17 @@ SELECT
 INSERT INTO
   games (game_code, description, minimum_players, reward)
 SELECT
-  'GCBETATST', 'The Anti-Social-Mob cache: Beta test', 2, 'BETA TEST SUCCESS. Gimme a B, gimme a E gimme a T gimma a A - BETA!';
+  'GCBETA', 'The Anti-Social-Mob cache: Beta test', 2, 'BETA TEST SUCCESS. Gimme a B, gimme a E gimme a T gimma a A - BETA!';
 
 INSERT INTO
     waypoint (game_code, name, location, radius)
 SELECT
-    'GCBETATST', 'Beta One', 'SRID=3857; POINT( -27.351467 153.0295)', 10;
+    'GCBETA', 'Beta One', 'SRID=3857; POINT(-27.35065 153.029383)', 10;
     
 INSERT INTO
     waypoint (game_code, name, location, radius)
 SELECT
-    'GCBETATST', 'Beta Two', 'SRID=3857; POINT(  -27.35215  153.029367)', 10;
+    'GCBETA', 'Beta Two', 'SRID=3857; POINT(-27.351167 153.029733)', 10;
     
     
 -- --------------------------------------------------------------------------------------------
@@ -45,19 +45,19 @@ SELECT
 INSERT INTO
   games (game_code, description, minimum_players, reward)
 SELECT
-  'GCGAMMATST', 'The Anti-Social-Mob cache: Gamma test', 3, 'A cuatro ... GAMMA TEST SUCCESS. Be the cache, see the cache, hold the cache - GAMMA GAMMA GAMMA!';
+  'GCGAMMA', 'The Anti-Social-Mob cache: Gamma test', 3, 'A cuatro ... GAMMA TEST SUCCESS. Be the cache, see the cache, hold the cache - GAMMA GAMMA GAMMA!';
 
 INSERT INTO
     waypoint (game_code, name, location, radius)
 SELECT
-    'GCGAMMATST', 'Gamma Uno', 'SRID=3857; POINT( -27.3511  153.0278)', 10;
+    'GCGAMMA', 'Gamma Uno', 'SRID=3857; POINT(-27.3509 153.02875)', 10;
     
 INSERT INTO
     waypoint (game_code, name, location, radius)
 SELECT
-    'GCGAMMATST', 'Gamma Dos', 'SRID=3857; POINT( -27.351833  153.027667)', 10;
+    'GCGAMMA', 'Gamma Dos', 'SRID=3857; POINT(-27.3505 153.028133)', 10;
     
 INSERT INTO
     waypoint (game_code, name, location, radius)
 SELECT
-    'GCGAMMATST', 'Gamma Tres', 'SRID=3857; POINT( -27.351517  153.028267)', 10;
+    'GCGAMMA', 'Gamma Tres', 'SRID=3857; POINT(-27.350867 153.027767)', 10;
