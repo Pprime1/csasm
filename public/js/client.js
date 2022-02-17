a5993f298925396c4cb2e5dced70608091643864
Pprime1 <43660222+Pprime1@users.noreply.github.com>
2022-02-17 12:20:04 +1000

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -138,7 +138,7 @@ socket.on("display-update", (display_information) => {
   if (display_information==null) {
       socket.emit('LOGTX',`${socket.id} :-> Trying to display a null table`); //clientlogdata should always be in the format of `${socket.id} :-> log message`
   } else { console.table(display_information);
-           socket.emit('LOGTX',`${socket.id} :-> Display ${display_information}`)}; //clientlogdata should always be in the format of `${socket.id} :-> log message`
+          socket.emit('LOGTX',`${socket.id} :-> Display ${display_information.ToString()}`)}; //clientlogdata should always be in the format of `${socket.id} :-> log message`
     //else skip this socket function
   
   displaytable=display_information;
